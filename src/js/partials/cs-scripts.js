649d68129164605a744b63f299b98110c650e468
Anton <anthonyustinoff@gmail.com>
2018-04-18 02:47:58 +0400

    filter style media

--- src/js/partials/cs-scripts.js
+++ src/js/partials/cs-scripts.js
@@ -1,8 +1,8 @@
 //Accordeon
 $('.js-cs-accordeon').find('.cs-accordeon__item').find('.cs-accordeon__title').on('click', function(){
     if($(this).parent().hasClass('is-open')){
-        $(this).parent().removeClass('is-open').find('.cs-accordeon__content').css('display', 'none');
+        $(this).parent().removeClass('is-open').find('.cs-accordeon__content').slideUp();
     }else{
-        $(this).parent().addClass('is-open').find('.cs-accordeon__content').removeAttr('style');
+        $(this).parent().addClass('is-open').find('.cs-accordeon__content').slideDown();
     }   
 });
