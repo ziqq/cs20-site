2e3694ad51aa4430972d67200ae653ded9a095b1
Anton <anthonyustinoff@gmail.com>
2018-07-19 12:49:55 +0400

    fix gulp js

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,35 +1,55 @@
-var gulp        = require('gulp');
-var config      = require('../config');
-var include     = require('gulp-include');
-var rename      = require('gulp-rename');
-var concat      = require('gulp-concat');
-var uglify      = require('gulp-uglify');
-var babel       = require('gulp-babel');
-var sourcemaps  = require('gulp-sourcemaps');
+var gulp = require('gulp');
+var config = require('../config');
+var include = require('gulp-include');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
 var runSequence = require('run-sequence');
 var browserSync = require('browser-sync');
 reload = browserSync.reload;
 
 gulp.task('js', function() {
-    return gulp.src(config.src.js + '/app.js')
-        .pipe(include())
-        .pipe(sourcemaps.init())
-        .on('error', function() {notify('Javascript include error');})
-        .pipe(babel())
-    // .pipe(uglify())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(config.production ? config.src.jsTemp + '/' : config.dest.js + '/'))
-        .pipe(reload({stream: true}));
+    return (
+        gulp
+            .src(config.src.js + '/app.js')
+            .pipe(include())
+            .pipe(sourcemaps.init())
+            .on('error', function() {
+                notify('Javascript include error');
+            })
+            .pipe(babel())
+            // .pipe(uglify())
+            .pipe(sourcemaps.write())
+            .pipe(
+                gulp.dest(
+                    config.production
+                        ? config.src.jsTemp + '/'
+                        : config.dest.js + '/'
+                )
+            )
+            .pipe(reload({ stream: true }))
+    );
 });
 
 gulp.task('js:libs', function() {
-    return gulp.src(config.src.js + '/libs.js')
+    return gulp
+        .src(config.src.js + '/libs.js')
         .pipe(include())
-        .on('error', function() {notify('Javascript include error');})
-        .pipe(rename({suffix: '.min', prefix : ''}))
+        .on('error', function() {
+            notify('Javascript include error');
+        })
+        .pipe(rename({ suffix: '.min', prefix: '' }))
         .pipe(uglify())
-        .pipe(gulp.dest(config.production ? config.src.jsTemp + '/' : config.dest.js + '/'))
-        .pipe(reload({stream: true}));
+        .pipe(
+            gulp.dest(
+                config.production
+                    ? config.src.jsTemp + '/'
+                    : config.dest.js + '/'
+            )
+        )
+        .pipe(reload({ stream: true }));
 });
 
 // gulp.task('js:dev', function () {
@@ -43,23 +63,21 @@ gulp.task('js:libs', function() {
 // });
 
 gulp.task('js:concat', function() {
-    return gulp.src([
-        config.src.jsTemp + '/libs.min.js',
-        config.src.jsTemp + '/main.js',
-        config.src.jsTemp + '/dev.js',
-    ])
+    return gulp
+        .src([
+            config.src.jsTemp + '/libs.min.js',
+            config.src.jsTemp + '/app.js'
+        ])
         .pipe(concat('app.js'))
-        .on('error', function() {notify('Javascript include error');})
+        .on('error', function() {
+            notify('Javascript include error');
+        })
         .pipe(uglify())
         .pipe(gulp.dest(config.dest.js + '/'));
 });
 
 gulp.task('js:all', function(js) {
-    runSequence(
-        'js:libs',
-        'js',
-        js
-    );
+    runSequence('js:libs', 'js', js);
 });
 
 gulp.task('js:build', function(js) {
